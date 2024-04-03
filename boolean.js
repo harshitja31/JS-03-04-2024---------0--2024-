451d72b78965998a9fe913dbb96b2adb8408bcfb
HARSHIT KUMAR JAIN <113702557+harshitja31@users.noreply.github.com>
2024-04-03 23:47:29 +0530

    hi

--- boolean.js
+++ boolean.js
@@ -4,4 +4,8 @@ let age = 19;
 let isAdult = true;
 console.log(isAdult);
 
-let isAdult = True;
+let adult = 'True';
+console.log(typeof isAdult);
+console.log(typeof adult);
+
+//let IsAdult = True;>>give error

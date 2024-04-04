cc74346b7b1fb4ba9c2b45d8f3af9eea6b8dbb7b
HARSHIT KUMAR JAIN <jainharshitkumar31@gmail.com>
2024-04-04 23:25:36 +0530

    Day-2

--- boolean.js
+++ boolean.js
@@ -4,17 +4,9 @@ let age = 19;
 let isAdult = true;
 console.log(isAdult);
 
-<<<<<<< HEAD
 // let adult = True; >> Gives essor
 
 let IsAdult = 'True';
 
 console.log(typeof isAdult); //boolean
 console.log(typeof IsAdult); //string
-=======
-let adult = 'True';
-console.log(typeof isAdult);
-console.log(typeof adult);
-
-//let IsAdult = True;>>give error
->>>>>>> 451d72b78965998a9fe913dbb96b2adb8408bcfb
